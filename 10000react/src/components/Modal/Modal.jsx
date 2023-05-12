f91afb03c9414241dc673d125161556f379bae6a
jsunbin <jsnb6n@gmail.com>
2023-05-13 05:43:47 +0900

    Modal.jsx - class -> className으로 변경

--- 10000react/src/components/Modal/Modal.jsx
+++ 10000react/src/components/Modal/Modal.jsx
@@ -5,11 +5,11 @@ import licat from "../../img/licat.png";
 export default function Modal() {
   return (
     <article id="modal">
-      <div class="modal-inner">
+      <div className="modal-inner">
         <h2>화이팅!!♥♥♥</h2>
         <h3>당신의 꿈을 응원합니다!</h3>
         <img src={licat} alt="licat" />
-        <button type="button" class="btn-colse">
+        <button type="button" className="btn-colse">
           종료하고 진짜 훈련하러 가기 GO!GO!
         </button>
       </div>
